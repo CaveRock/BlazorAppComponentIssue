07a53ef31b0bca99bf837dbaf51cdcb2194389c5
Oliver Shepherd <oliver@bitdirect.co.za>
2023-12-12 16:32:09 +0200

    Add project files.

---
+++ BlazorAppComponentIssue.Client/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppComponentIssue.Client
+{
+    public class FormModel
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+    }
+}
